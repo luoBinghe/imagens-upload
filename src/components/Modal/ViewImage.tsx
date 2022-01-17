9a416efca14bf9a6ae8cff9f6f617f92d9d9a8db
guayana <isabelaguayanaz@gmail.com>
2022-01-17 17:44:19 -0300

    arrumando o tamanho da imagem

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -23,9 +23,9 @@ export function ModalViewImage({
   return (
     <Modal isOpen onClose={onClose} >
       <ModalOverlay />
-      <ModalContent>
+      <ModalContent maxWidth="900px" maxHeight="600px" bgColor="pGray.800">
         <ModalBody>
-          <Image />
+          <Image src={imgUrl} alt="image" />
         </ModalBody>
         <ModalFooter />
       </ModalContent>
